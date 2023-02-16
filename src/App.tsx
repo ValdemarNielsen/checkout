2f908f7d80e84ccf80dfd74fff813660393b203d
Christian H <christian@hyltoft.net>
2023-02-16 21:20:04 +0100

    works now, display running

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
-import {FormEvent, useState} from 'react'
-import {BrowserRouter as Router, Route, Link} from "react-router-dom"
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Shop from './pages/shop'
-import Checkoutç from './pages/checkout'
-
+import { FormEvent, useState } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import reactLogo from "./assets/react.svg";
+import "./styles/App.css";
+import Shop from "./pages/shop";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-      <html lang="en">
-      
-    <div className="App">
-    <Shop/>
-    </div>
-      </html>
-  )
+    <html lang="en">
+      <div className="App">
+        <Shop />
+      </div>
+    </html>
+  );
 }
 
-export default App
+export default App;
--- src/pages/shop.tsx
+++ src/pages/shop.tsx
@@ -1,10 +1,9 @@
 //Description: This is the shop page, where the user chooses which items they want to proceed to checkout with.
 import React from "react";
 import products from "../assets/products";
-import shopstyle from "../styles/shop.css";
 export default function Shop() {
   return (
-    <div className="shopstyle">
+    <div>
       <h1>Welcome To The Shop</h1>
       <div>
         {products.map((product) => (
