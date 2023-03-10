a0cd0346bf33286bc2925366ec7460e6a4812f34
Christian H <christian@hyltoft.net>
2023-03-10 14:00:53 +0100

    small changes to shoptsx and Forms

--- src/pages/shop.tsx
+++ src/pages/shop.tsx
@@ -1,61 +1,61 @@
 //Description: This is the shop page, where the user chooses which items they want to proceed to checkout with.
 import React from "react";
-import products, {BasketItems, itemDict} from "../assets/products";
-import {useState} from "react";
+import products, { BasketItems, itemDict } from "../assets/products";
+import { useState } from "react";
 import "../styles/shop.css";
-import {Navigate} from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 export default function Shop() {
-    const [basket, setBasket] = useState<BasketItems[]>([
-        {
-            ...itemDict["clear-whey-100"],
-            quantity: 2,
-            giftWrap: false,
-        },
-        {
-            ...itemDict["valle-protion-whey-100-vanilla"],
-            quantity: 1,
-            giftWrap: true,
-        },
-        {
-            ...itemDict["valle-protein-whey-100-chocolate"],
-            quantity: 2,
-            giftWrap: false,
-        },
-        {
-            ...itemDict["fish-oil-1000-120"],
-            quantity: 1,
-            giftWrap: false,
-        },
-    ] as BasketItems[]);
+  const [basket, setBasket] = useState<BasketItems[]>([
+    {
+      ...itemDict["clear-whey-100"],
+      quantity: 2,
+      giftWrap: false,
+    },
+    {
+      ...itemDict["valle-protion-whey-100-vanilla"],
+      quantity: 1,
+      giftWrap: true,
+    },
+    {
+      ...itemDict["valle-protein-whey-100-chocolate"],
+      quantity: 2,
+      giftWrap: false,
+    },
+    {
+      ...itemDict["fish-oil-1000-120"],
+      quantity: 1,
+      giftWrap: false,
+    },
+  ] as BasketItems[]);
 
-    const incrementBasketItem = (id: string) => {
-        const newBasket = basket.map((item) => {
-            if (item.id === id) {
-                return {
-                    ...item,
-                    quantity: item.quantity + 1,
-                };
-            }
-            return item;
-        });
-        setBasket(newBasket);
-    };
+  const incrementBasketItem = (id: string) => {
+    const newBasket = basket.map((item) => {
+      if (item.id === id) {
+        return {
+          ...item,
+          quantity: item.quantity + 1,
+        };
+      }
+      return item;
+    });
+    setBasket(newBasket);
+  };
 
-    const decrementBasketItem = (id: string) => {
-        const newBasket = basket.map((item) => {
-            if (item.id === id) {
-                if (item.quantity >= 1) {
-                    return {
-                        ...item,
-                        quantity: item.quantity - 1,
-                    };
-                }
-            }
-            return item;
-        });
-        setBasket(newBasket);
-    };
+  const decrementBasketItem = (id: string) => {
+    const newBasket = basket.map((item) => {
+      if (item.id === id) {
+        if (item.quantity >= 1) {
+          return {
+            ...item,
+            quantity: item.quantity - 1,
+          };
+        }
+      }
+      return item;
+    });
+    setBasket(newBasket);
+  };
 
   return (
     <div>
@@ -71,7 +71,7 @@ export default function Shop() {
                 justifyContent: "space-between",
               }}
             >
-              <img src={product.image} />
+              <img src={product.image} className="imagepadding" />
               <div
                 style={{
                   display: "flex",
@@ -93,9 +93,7 @@ export default function Shop() {
                     <DecrementButton
                       onClick={() => decrementBasketItem(product.id)}
                     />
-                    <div className="quantityspacer">
-                      <p>{product.quantity}</p>
-                    </div>
+                    <p>{product.quantity}</p>
                     <IncrementButton
                       onClick={() => incrementBasketItem(product.id)}
                     />
@@ -121,39 +119,39 @@ export default function Shop() {
 }
 
 //Use useState to update the quantity of the products when using decrement and increment buttons.
-function DecrementButton({onClick}: { onClick: () => void }) {
-    return (
-        <button onClick={onClick} className="buttondecrement">
-            -
-        </button>
-    );
+function DecrementButton({ onClick }: { onClick: () => void }) {
+  return (
+    <button onClick={onClick} className="buttondecrement">
+      -
+    </button>
+  );
 }
 
-function IncrementButton({onClick}: { onClick: () => void }) {
-    return (
-        <button onClick={onClick} className="buttonincrement">
-            +
-        </button>
-    );
+function IncrementButton({ onClick }: { onClick: () => void }) {
+  return (
+    <button onClick={onClick} className="buttonincrement">
+      +
+    </button>
+  );
 }
 
 //Make the totalPriceWRebate function which calculates the total price of the basket and check the quantity of each product to see if the rebate applies.
 
 function totalPriceWRebate(basket: BasketItems[]) {
-    let rabatGiven = 0;
-    let totalPrice = 0;
-    basket.forEach((item) => {
-        totalPrice += item.price * item.quantity;
-        if (item.quantity >= item.rebateQuantity) {
-            totalPrice -= (item.price * item.quantity * item.rebatePercent) / 100;
-        }
-    });
-    if (totalPrice >= 300 && rabatGiven == 0) {
-        totalPrice = totalPrice * 0.9
-        rabatGiven = 1;
+  let rabatGiven = 0;
+  let totalPrice = 0;
+  basket.forEach((item) => {
+    totalPrice += item.price * item.quantity;
+    if (item.quantity >= item.rebateQuantity) {
+      totalPrice -= (item.price * item.quantity * item.rebatePercent) / 100;
     }
-   // return totalPrice;
-    return (Math.round(totalPrice * 100) / 100).toFixed(2);
+  });
+  if (totalPrice >= 300 && rabatGiven == 0) {
+    totalPrice = totalPrice * 0.9;
+    rabatGiven = 1;
+  }
+  // return totalPrice;
+  return (Math.round(totalPrice * 100) / 100).toFixed(2);
 }
 
 /*
@@ -171,32 +169,31 @@ const [screen, setScreen] = useState(0);
 }
 */
 
-
 //Function that calculates the amount of rebate the user gets.
 function rebateAmount(basket: BasketItems[]) {
-    let rebate = 0;
-    let totalPrice = 0;
-    let rabat = 0;
-    let extrarabate = 0;
-    basket.forEach((item) => {
-        if (item.quantity >= item.rebateQuantity) {
-            rebate += (item.price * item.quantity * item.rebatePercent) / 100;
-        }
-    });
-
-    basket.forEach((item) => {
-        totalPrice += item.price * item.quantity;
-        if (item.quantity >= item.rebateQuantity) {
-            totalPrice -= (item.price * item.quantity * item.rebatePercent) / 100;
-        }
-    });
-
-    if (totalPrice >= 300 && rabat == 0) {
-        extrarabate = totalPrice - (totalPrice * 0.9)
-        rabat = 1;
+  let rebate = 0;
+  let totalPrice = 0;
+  let rabat = 0;
+  let extrarabate = 0;
+  basket.forEach((item) => {
+    if (item.quantity >= item.rebateQuantity) {
+      rebate += (item.price * item.quantity * item.rebatePercent) / 100;
     }
-     // return rebate + extrarabate;
-    return (Math.round(rebate+extrarabate * 100) / 100).toFixed(2);
+  });
+
+  basket.forEach((item) => {
+    totalPrice += item.price * item.quantity;
+    if (item.quantity >= item.rebateQuantity) {
+      totalPrice -= (item.price * item.quantity * item.rebatePercent) / 100;
+    }
+  });
+
+  if (totalPrice >= 300 && rabat == 0) {
+    extrarabate = totalPrice - totalPrice * 0.9;
+    rabat = 1;
+  }
+  // return rebate + extrarabate;
+  return (Math.round(rebate + extrarabate * 100) / 100).toFixed(2);
 }
 
 //Function discountBox, shows a box with a button to apply a discount code from one of the different codes in the discountCodes array.
