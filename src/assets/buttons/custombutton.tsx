46a4c5c0c623eb8719ce2f83b772c32201998eae
Christian H <christian@hyltoft.net>
2023-03-28 14:58:14 +0200

    massive merge conflict resolved

--- src/assets/buttons/custombutton.tsx
+++ src/assets/buttons/custombutton.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-
 interface CustomButtonProps {
   label: string;
   disabled?: boolean;
   onClick?: () => void;
 }
-
 const CustomButton = ({ label, disabled, onClick }: CustomButtonProps) => {
   return (
     <button disabled={disabled} onClick={onClick}>
@@ -15,12 +13,37 @@ const CustomButton = ({ label, disabled, onClick }: CustomButtonProps) => {
   );
 };
 
-export const NextButton = ({ disabled }: { disabled: boolean }) => {
+export const SubmitButton = ({ disabled }: { disabled: boolean }) => {
   const navigate = useNavigate();
 
   const handleOnClick = () => {
     if (!disabled) {
       navigate("/payment");
+    }
+  };
+
+  return (
+    <CustomButton label="Submit" disabled={disabled} onClick={handleOnClick} />
+  );
+};
+export const ConfirmButton = ({ disabled }: { disabled: boolean }) => {
+  const navigate = useNavigate();
+  const handleOnClick = () => {
+    if (!disabled) {
+      navigate("/confirmation");
+    }
+  };
+
+  return (
+    <CustomButton label="Confirm" disabled={disabled} onClick={handleOnClick} />
+  );
+};
+
+export const NextButton = ({ disabled }: { disabled: boolean }) => {
+  const navigate = useNavigate();
+  const handleOnClick = () => {
+    if (!disabled) {
+      navigate("/checkout");
     }
   };
 
@@ -32,16 +55,13 @@ export const NextButton = ({ disabled }: { disabled: boolean }) => {
     />
   );
 };
-
 export const BackButton = ({ disabled }: { disabled: boolean }) => {
   const navigate = useNavigate();
-
   const handleOnClick = () => {
     if (!disabled) {
       navigate("/");
     }
   };
-
   return (
     <CustomButton label="Back" disabled={disabled} onClick={handleOnClick} />
   );
