0ec60b2c4e29e6b3a51e6bc7eba72b230d292bbe
Christian H <christian@hyltoft.net>
2023-04-10 00:23:51 +0200

    placeholder

--- src/assets/EmailWelcome.tsx
+++ src/assets/EmailWelcome.tsx
@@ -42,6 +42,7 @@ function EmailWelcome() {
               <form onSubmit={handleSubmit}>
                 <input
                   type="email"
+                  placeholder="Enter your email"
                   value={email}
                   onChange={(event) => setEmail(event.target.value)}
                   required
