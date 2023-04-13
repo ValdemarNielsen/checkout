2dfefc9b9fc3cec80fc57bb2b50b5fe23b2b1b76
Christian H <christian@hyltoft.net>
2023-04-13 12:53:08 +0200

    emailwelcome prompt finished

--- src/assets/EmailWelcome.tsx
+++ src/assets/EmailWelcome.tsx
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from "react";
+import css from "@emotion/css";
+import { BeatLoader } from "react-spinners";
 import "../styles/EmailWelcome.css";
+
 function EmailWelcome() {
   const [show, setShow] = useState(false);
   const [email, setEmail] = useState("");
+  const [showText, setShowText] = useState(false);
+  const [canSubmit, setCanSubmit] = useState(false);
+  const [textVisible, setTextVisible] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const hasTakenAction = localStorage.getItem("hasTakenAction");
@@ -14,14 +21,32 @@ function EmailWelcome() {
 
   const handleSubmit = (event: { preventDefault: () => void }) => {
     event.preventDefault();
-    // Here can we call the action if needed ;))
-    localStorage.setItem("hasTakenAction", true.toString());
-    setShow(false);
+    if (canSubmit) {
+      setIsLoading(true);
+      setTimeout(() => {
+        localStorage.setItem("hasTakenAction", true.toString());
+        setIsLoading(false);
+        setShowText(true);
+        setTextVisible(true);
+      }, 2000);
+    }
   };
 
   const handleNoThanks = () => {
     localStorage.setItem("hasTakenAction", true.toString());
     setShow(false);
+  };
+
+  const handleClose = () => {
+    localStorage.setItem("hasTakenAction", true.toString());
+    setShow(false);
+  };
+
+  const handleInputChange = (event: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
+    setEmail(event.target.value);
+    setCanSubmit(event.target.value !== "");
   };
 
   return (
@@ -31,7 +56,7 @@ function EmailWelcome() {
           <div className="modal-content">
             <div className="modal-header">
               <h3 className="modal-title">
-                Subscribe to our newsletter and recieve 10% COUPON CODE!
+                Subscribe to our newsletter and receive 10% COUPON CODE!
               </h3>
             </div>
             <div className="modal-body">
@@ -39,19 +64,41 @@ function EmailWelcome() {
                 Be the first to know about our latest products, exclusive offers
                 and much more!
               </p>
-              <form onSubmit={handleSubmit}>
-                <input
-                  type="email"
-                  placeholder="Enter your email"
-                  value={email}
-                  onChange={(event) => setEmail(event.target.value)}
-                  required
-                />
-                <button type="submit">Sign me up!</button>
-              </form>
-              <button className="close-btn" onClick={handleNoThanks}>
-                No thanks
-              </button>
+              {!showText && (
+                <form onSubmit={handleSubmit}>
+                  <input
+                    type="email"
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={handleInputChange}
+                    required
+                  />
+                  {isLoading ? (
+                    <div className="loading-spinner">
+                      <BeatLoader color="#000000" loading={true} size={10} />
+                    </div>
+                  ) : (
+                    <button type="submit" disabled={!canSubmit}>
+                      Sign me up!
+                    </button>
+                  )}
+                </form>
+              )}
+              {showText && (
+                <p className="hidden-text">
+                  USE CODE: PROTEIN10 to get 10% off your next order!
+                </p>
+              )}
+              {textVisible && (
+                <button className="close-btn" onClick={handleClose}>
+                  Close
+                </button>
+              )}
+              {!textVisible && (
+                <button className="close-btn" onClick={handleNoThanks}>
+                  No thanks
+                </button>
+              )}
             </div>
           </div>
         </div>
