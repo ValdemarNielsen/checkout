f7f24289e1e1af51aa8f9b7a665f4f118d8336b1
seburu <sebynoah@gmail.com>
2023-05-01 10:50:50 +0200

    Confirmation and payment update

--- src/pages/confirmation.tsx
+++ src/pages/confirmation.tsx
@@ -4,10 +4,17 @@ type ConfirmationProps = {
   navigate: (page: string) => void;
 };
 
+
+
+
 function Confirmation(props: ConfirmationProps) {
   return (
     <>
       <h1>Confirmation</h1>
+        <div>
+            <h1>Thank you for your order!</h1>
+            <p>Your order number is: #DK411</p>
+        </div>
     </>
   );
 }
