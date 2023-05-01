bc4dd4623949b4fcfccd95b29b1818f577942703
Christian H <christian@hyltoft.net>
2023-05-01 14:44:03 +0200

    confirmation page

--- src/pages/confirmation.tsx
+++ src/pages/confirmation.tsx
@@ -1,20 +1,28 @@
 import React from "react";
+import ".././styles/shop.css";
 
 type ConfirmationProps = {
   navigate: (page: string) => void;
 };
 
-
-
-
 function Confirmation(props: ConfirmationProps) {
   return (
     <>
-      <h1>Confirmation</h1>
+      <div
+        className="layoutMaster container"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <h1 style={{ padding: "0.5rem" }}>CONFIRMATION</h1>
         <div>
-            <h1>Thank you for your order!</h1>
-            <p>Your order number is: #DK411</p>
+          <h1>Thank you for your order!</h1>
+          <p>Your order number is: #DK411</p>
         </div>
+        <span>A confirmation email has been sent to your mail</span>
+      </div>
     </>
   );
 }
